6367b1bc0929aad75d4ec9f5fac4b6c8db39ea0f
hamdard345 <58349754+hamdard345@users.noreply.github.com>
2023-12-12 00:49:27 +0000

    an abstract demo

---
+++ src/main/java/com/mycompany/javapractice/AbstractDemo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.javapractice;
+
+/**
+ *
+ * @author HP
+ */
+public class AbstractDemo extends MyAbstractClass {
+    @Override
+    void print(){
+        System.out.println("in print method");
+    }
+}
---
+++ src/main/java/com/mycompany/javapractice/JavaPractice.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javapractice;
+
+/**
+ *
+ * @author HP
+ */
+public class JavaPractice {
+
+    public static void main(String[] args) {
+        AbstractDemo obj = new AbstractDemo();
+        obj.print();
+        obj.display();
+    }
+}
